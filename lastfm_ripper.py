8c5852ff3bea370abc1cb5599c850e6515f0a8e3
node <node1729@gmail.com>
2019-03-07 12:34:58 -0600

    Finished Lastfm support

--- lastfm_ripper.py
+++ lastfm_ripper.py
@@ -22,12 +22,13 @@ def lastfmAPIReq(URL):
     output = json.loads(r.data.decode("utf-8"))
     return output
 
-def user_get_albums(user):
-    output = lastfmAPIReq("?method=user.gettopalbums&user=" + user + "&period=1month")
+def user_get_albums(user, period, limit):
+    output = lastfmAPIReq("?method=user.gettopalbums&user=" + user + "&period=" + period + "&limit=" + limit)
     test_file = open("lastfm.json", "w")
     json.dump(output, test_file, indent=4)
+    test_file.close() # close file to prevent JSONDecodeError
     rip_album_art(True)
-
+    
 def rip_album_art(outname=False):
     infile = open("lastfm.json")
     in_dict = json.load(infile)
@@ -41,11 +42,35 @@ def rip_album_art(outname=False):
             urllib.request.urlretrieve(img, artist + " - " + album_title + ".jpg")
             print("Saved under \"photos/" + artist + " - " + album_title + ".jpg\"")
         else:
-            urllib.request.urlretrieve(img, str(x) + ".jpg")
-            print("Saved under \"photos/" + str(x) + ".jpg\"")
+            urllib.request.urlretrieve(img, str(x).zfill(3) + ".jpg")
+            print("Saved under \"photos/" + str(x).zfill(3) + ".jpg\"")
 
-            
-if len(sys.argv) == 2:
-    user_get_albums(sys.argv[1])
+    infile.close()
+if len(sys.argv) == 1:
+    print("Invalid syntax, please input at least " + sys.argv[0] + " [user] [period](optional, default=1month) [limit](optional, default=25)")
+
+if len(sys.argv) > 1:
+    period = "1month"
+    limit = "25"
+    period_array = ["7day", "1month", "3month", "6month", "12month", "overall"]
+    try:
+        period = sys.argv[2]
+        if period not in period_array:
+            raise PeriodInvalidError("Period not in {}, given".format(period_array))
+    except IndexError:
+        period = "1month"
+
+    try:
+        limit = sys.argv[3]
+        if int(limit) > 100 or int(limit) < 1:
+            raise LimitInvalidError("Limit invalid, given {}, 1 <= limit <= 100 expected".format(limit))
+    except IndexError:
+        limit = "25"
+
+    user_get_albums(sys.argv[1], period, limit)
+
+
+
+
 
 
