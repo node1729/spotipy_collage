b9aa1636112669a1ad3c7f9e7f8a82e62500338b
node1729 <node1729@gmail.com>
2019-03-07 13:08:08 -0600

    Implement ripping feature

--- lastfm_ripper.py
+++ lastfm_ripper.py
@@ -3,11 +3,15 @@ import urllib3
 import certifi
 import urllib.request
 import spotipy
+import sys
+import os
 
 settings_file = open("properties.json")
 settings_dict = json.load(settings_file)
 
 API_KEY = settings_dict["LAST_FM_API_KEY"]
+
+os.chdir("photos")
 
 def lastfmAPIReq(URL):
     http = urllib3.PoolManager(
@@ -22,6 +26,26 @@ def user_get_albums(user):
     output = lastfmAPIReq("?method=user.gettopalbums&user=" + user + "&period=1month")
     test_file = open("lastfm.json", "w")
     json.dump(output, test_file, indent=4)
+    rip_album_art(True)
 
-user_get_albums("node1729")
+def rip_album_art(outname=False):
+    infile = open("lastfm.json")
+    in_dict = json.load(infile)
+    x = 0
+    for item in in_dict["topalbums"]["album"]:
+        x += 1
+        img = item["image"][3]["#text"]
+        artist = item["artist"]["name"]
+        album_title = item["name"]
+        if not outname:
+            urllib.request.urlretrieve(img, artist + " - " + album_title + ".jpg")
+            print("Saved under \"photos/" + artist + " - " + album_title + ".jpg\"")
+        else:
+            urllib.request.urlretrieve(img, str(x) + ".jpg")
+            print("Saved under \"photos/" + str(x) + ".jpg\"")
+
+            
+if len(sys.argv) == 2:
+    user_get_albums(sys.argv[1])
+
 
